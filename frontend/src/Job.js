b1a96888e8a36e41dcf3833692fabe27cebb2ba4
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-07 20:24:10 -0600

    all 'apply' buttons working

--- frontend/src/Job.js
+++ frontend/src/Job.js
@@ -1,15 +1,27 @@
 import React from "react";
 import './Job.css'
+import useLocalStorage from "./hooks/useLocalStorage";
 
 
-const Job = ({ id, title, salary, equity }) => {
+const Job = ({ id, title, salary, equity, submitApplication2 }) => {
+
+  const [storeToken, removeToken, verifyToken, retrieveToken] = useLocalStorage();
+
+  const handleApply = async (e) => {
+    const id = e.target.parentNode.getAttribute('id')
+    console.log(id)
+    const decodedToken = await verifyToken()
+    console.log(decodedToken.username)
+    submitApplication2(decodedToken.username, id)
+  }
+
 
   return (
     <div className="jobCard" id={id}> 
       <h4>{title}</h4>
       <p>Salary: {salary}</p>
       <p>Equity: {equity}</p>
-      <button>Apply</button>
+      <button onClick={handleApply}>Apply</button>
     </div>
   )
 }
